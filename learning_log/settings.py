01887b92f46239355c407018ea76d5139500295e
M1sterZag <maximzaglubotsky@gmail.com>
2024-09-09 17:22:16 +0000

    add new functionality

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Мои приложения
+    'users',
     'learning_logs',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login/'
