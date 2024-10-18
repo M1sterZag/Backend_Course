447e429e19c301917696f3fc01c2dfec6cb59e13
Maxim Zaglubotsky <91866807+M1sterZag@users.noreply.github.com>
2024-10-18 11:46:03 +0000

    changed styles and added forms

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -8,10 +8,26 @@ class TopicForm(forms.ModelForm):
         fields = ['text']
         labels = {'text': ''}
 
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({
+            'class': 'form-control',  # Применяем класс Bootstrap
+            'placeholder': 'Enter topic name'  # Плейсхолдер
+        })
+
+
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['text']
-        labels = {'text': 'Entry:'}
-        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+        labels = {'text': ''}
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',  # Применяем класс Bootstrap
+                'rows': 5,  # Высота текстового поля
+                'placeholder': 'Write your entry here...'  # Плейсхолдер
+            })
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
